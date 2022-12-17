1cc33eff4174acc9ce17ea8743fb9f842d8d154b
Saehwa95 <tpghk1002@naver.com>
2022-12-17 16:33:00 +0900

    commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from '../components/Header'
+import RegisterForm from "../components/user/RegisterForm";
+
+const Register = () => {
+  return (
+    <>
+    <Header />
+    <RegisterForm />
+    </>
+  );
+};
+
+export default Register;
