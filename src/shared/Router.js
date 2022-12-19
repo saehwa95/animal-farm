4bcefda41742b61c9d6028e1b3d306bdfb4524e2
Saehwa95 <tpghk1002@naver.com>
2022-12-19 14:16:33 +0900

    라우터 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
-import Register from "../pages/Register"
-import Home from "../pages/Home"
-import Login from "../pages/Login"
-import PostCreate from "../pages/PostCreate"
-import PostDetail from "../pages/PostDetail"
-import PostUpdate from "../pages/PostUpdate"
+import Register from "../pages/Register";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PostCreate from "../pages/PostCreate";
+import PostDetail from "../pages/PostDetail";
+import PostUpdate from "../pages/PostUpdate";
 
 const Router = () => {
   return (
@@ -18,7 +18,7 @@ const Router = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={<PostCreate />} />
-          <Route path="/detail/:id" element={<PostDetail />} />
+          {/* <Route path="/detail/:id" element={<PostDetail />} /> */}
           {/* <Route path="/update/:id" element={<PostUpdate/> } /> */}
         </Routes>
       </Layout>
