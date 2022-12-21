f2c089adfde39dbddf2ce365c68a98fbd8bd903a
idenfel <110656247+hanul-dev@users.noreply.github.com>
2022-12-22 00:02:47 +0900

    add 404 page

---
+++ src/pages/404/NotFound.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.load}>
+        <div> </div>
+        <div>R</div>
+        <div>O</div>
+        <div>R</div>
+        <div>R</div>
+        <div>E</div>
+        <div> </div>
+        <div>E</div>
+        <div>G</div>
+        <div>A</div>
+        <div>P</div>
+        <div> </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -9,21 +9,24 @@ import PostCreate from "../pages/PostCreate";
 import PostDetail from "../pages/PostDetail";
 import PostUpdate from "../pages/PostUpdate";
 import Intro from "../pages/Intro";
+import NotFound from '../pages/404/NotFound';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Layout>
         <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/create" element={<PostCreate />} />
-          <Route path="/detail/:id" element={<PostDetail />} />
-          <Route path="/update/:id" element={<PostUpdate />} />
+          <Layout>
+            <Route path="/" element={<Intro />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create" element={<PostCreate />} />
+            <Route path="/detail/:id" element={<PostDetail />} />
+            <Route path="/update/:id" element={<PostUpdate />} />
+            <Route path="/*" element={<NotFound />} />
+          </Layout>
         </Routes>
-      </Layout>
+      
     </BrowserRouter>
   );
 };
