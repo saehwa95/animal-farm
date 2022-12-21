850ed638dbaa5037fc7051783955831248414300
Aekyoung_Kwon <116799513+buddesign@users.noreply.github.com>
2022-12-21 22:07:42 +0900

    1221_update 수정

--- src/pages/PostUpdate.jsx
+++ src/pages/PostUpdate.jsx
@@ -1,47 +1,36 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import TextBox from "../elements/TextBox";
 import Wrapper from "../elements/Wrapper";
 import MdPrimaryBtn from "../elements/MdPrimaryBtn";
 import { __addPosts } from "../redux/modules/postsSlice";
+import { useParams } from "react-router-dom";
 
 const PostUpdate = () => {
   const dispatch = useDispatch();
-  const [input, setInput] = useState("");
+  //const [input, setInput] = useState("");
   const [image, setImage] = useState([]);
-  const [img, setImg] = useState([]);
+  //const [img, setImg] = useState([]);
+  const [retext, setRetext] = useState("");
+  const state = useSelector((state) => state.postsSlice);
+  console.log(state);
+  const params = useParams(state.posts.postId);
+  console.log(params);
 
-  //text input
-  const inputHandler = (e) => {
-    const value = e.target.value;
-    setInput({ ...input, text: value });
-  };
+  // 원본 텍스트 내용
+  useEffect(() => {});
 
-  //이미지 리더
-  const onChangeSelectImages = async (e) => {
-    const img = e.target.files;
-
-    let fileURLs = [];
-    let filesLength = img.length > 5 ? 5 : img.length;
-    for (let i = 0; i < filesLength; i++) {
-      let file = img[i];
-
-      let reader = new FileReader();
-      reader.onload = () => {
-        fileURLs[i] = reader.result;
-        setImg([...fileURLs]);
-      };
-      reader.readAsDataURL(file);
-    }
-    setImage([...img]);
+  //수정된 내용 담기
+  const contentChangeHandler = (e) => {
+    setRetext(e.target.value);
   };
 
   //등록하기
   const onSubmitHandler = (e) => {
     e.preventDefault();
     let formData = new FormData();
-    formData.append("text", input.text);
+    formData.append("text", retext.text);
     for (let i = 0; i < image.length; i++) {
       formData.append("images", image[i]);
     }
@@ -52,8 +41,8 @@ const PostUpdate = () => {
     <>
       <Wrapper>
         <Stform>
-          <ImgBox>
-            {/* {console.log(image)} */}
+          {state.text}
+          {/* <ImgBox>
             {img.map((img, i) => (
               <div key={i}>
                 <img
@@ -64,8 +53,8 @@ const PostUpdate = () => {
                 />
               </div>
             ))}
-          </ImgBox>
-          <ImgButton For="file" onChange={onChangeSelectImages}>
+          </ImgBox> */}
+          {/* <ImgButton For="file" onChange={onChangeSelectImages}>
             사진 첨부하기
             <input
               type="file"
@@ -74,17 +63,15 @@ const PostUpdate = () => {
               id="fileUpload"
               name="images"
               style={{ display: "none" }}
-              required
             />
-          </ImgButton>
+          </ImgButton> */}
           <TextBox
-            onChange={inputHandler}
+            onChange={contentChangeHandler}
             width="100%"
             height="240px"
             type="text"
             name="text"
-            value={input.text}
-            required
+            value={retext.text}
           ></TextBox>
           <div style={{ margin: "20px" }}>
             <MdPrimaryBtn onClick={onSubmitHandler}>등록하기</MdPrimaryBtn>
