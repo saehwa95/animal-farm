0679f662315a78644341bc5f355157dd644784e5
idenfel <110656247+hanul-dev@users.noreply.github.com>
2022-12-22 14:42:06 +0900

    feat comment 미완성

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{ useEffect } from 'react';
 
 import Wrapper from '../elements/Wrapper';
 import styled from 'styled-components';
@@ -10,33 +10,36 @@ import MdPrimaryBtn from '../elements/MdPrimaryBtn';
 import MdSecondaryBtn from '../elements/MdSecondaryBtn';
 import InnerBox from '../elements/InnerBox'
 import CommentContainer from '../components/detail/CommentContainer';
-import { __getDetailPost } from "../redux/modules/postsSlice";
+import { __getDetailPost, __deletePosts } from "../redux/modules/postsSlice";
 import { useSelector, useDispatch } from 'react-redux';
 
 const PostDetail = () => {
-  const postId = useParams();
-  const dispatch = useDispatch();
+  const {id} = useParams();
+  const dispatch = useDispatch()
+  
+  const {posts} = useSelector((state) => state.postsSlice);
 
-  // console.log(postId);
-  // console.log(typeof Number(postId.id));
-  // console.log(Number(postId.id))
+  const { text, postId, imageUrl } = posts[0] ?? {
+    text: "",
+    postId: "",
+    imagesUrl: []
+  };
 
-  // console.log(text);
 
   useEffect(() => {
-    dispatch(__getDetailPost({postId: postId}));
+    dispatch(__getDetailPost({postId: id}));
   },[]);
 
   return (
     <Wrapper>
-      <PostContainer/>
+      <PostContainer post={posts[0]}/>
       <InnerBox padding="40px 0" gap="1.3em" justifyContent="center" >
-        <NavLink to={`/update/${postId.id}`}>
+        <NavLink to={`/update/${postId}`}>
           <MdPrimaryBtn>수정하기</MdPrimaryBtn>
         </NavLink>
-        <MdSecondaryBtn>삭제하기</MdSecondaryBtn>
+        <MdSecondaryBtn onClick={() => dispatch(__deletePosts({postId}))}>삭제하기</MdSecondaryBtn>
       </InnerBox>
-      <CommentContainer text={""}/>
+      <CommentContainer postId={id}/>
     </Wrapper>
   );
 };
